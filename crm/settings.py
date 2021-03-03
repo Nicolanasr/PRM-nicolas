72194d84a21b807830e51d98a9596530ccced01f
nicolnasr <38070735+nicolnasr@users.noreply.github.com>
2021-03-03 14:52:43 +0200

    P

--- crm/settings.py
+++ crm/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'SMTP.GMAIL.COM'
 EMAIL_HOST_USER = 'nicolasnasr69@gmail.com'
-EMAIL_HOST_PASSWORD = 'coco3awn'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
